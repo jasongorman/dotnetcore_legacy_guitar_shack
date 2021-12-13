16308e3e53ddfcc672b6305ee69aef3aafda14f4
Jason Gorman <jason.gorman@codemanship.com>
2021-12-13 09:58:13 +0000

    Initial commit

---
+++ LegacyGuitarShack.App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GuitarShack.App
+{
+    static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var alert = new TestReorderAlert();
+            await new StockMonitor(alert).OnSale(Convert.ToInt32(args[0]), Convert.ToInt32(args[1]));
+        }
+    }
+
+    internal class TestReorderAlert : IReorderAlert
+    {
+        public void Send(Product product)
+        {
+            Console.WriteLine($"Please reorder product {product.id} - {product.description}");
+        }
+    }
+}
